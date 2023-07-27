c8781ad31d0fa1986fb3146fba6b5ef6c3ce15a7
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-07-27 22:16:23 +0300

    Добавьте файлы проекта.

---
+++ Sum Strings as Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+public static class Kata
+{
+    public static void Main()
+    {
+        // Test
+        var t = sumStrings("432341231", "567658768");
+        // ...should return "999999999"
+    }
+
+    public static string sumStrings(string a, string b)
+    {
+        if (a == "") 
+            a = "0";
+
+        return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+    }
+}
